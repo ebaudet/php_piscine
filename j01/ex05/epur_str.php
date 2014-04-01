f7c3f01b0f6af5a0693fda5456ccf38efdef7bbd
Emilien Baudet <emilien.baudet@gmail.com>
2014-04-01 17:44:48 +0200

    ex03 - j01

---
+++ j01/ex05/epur_str.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<?php
+	function multi_explode($delimiters, $string)
+	{
+		$ready = str_replace($delimiters, $delimiters[0], $string);
+		$launch = explode($delimiters[0], $ready);
+		return $launch;
+	}
+
+	function ft_plit($string)
+	{
+		$tab = multi_explode(array(" ", "\t", "\n"), $string);
+		for ($i = 0; isset($tab[$i]) ; $i++)
+		{
+			if ($tab[$i] == "")
+				unset($tab[$i]);
+		}
+		$tab = array_merge($tab);
+		return $tab;
+	}
+
+	function epur_str($string)
+	{
+		$tab = ft_plit($string);
+		print_r($tab);
+		for ($i = 0; isset($tab[$i]); $i++)
+		{
+			if ($i != 0)
+				echo " ";
+			echo "$tab[$i]";
+		}
+	}
+
+if (isset($argv[1]))
+{
+	epur_str($argv[1]);
+	echo "\n";
+}
+
+?>
