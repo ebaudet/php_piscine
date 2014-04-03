72be52ccfbb69089b565696205195b0f1f237c22
Emilien Baudet <emilien.baudet@gmail.com>
2014-04-03 18:22:45 +0200

    modif

--- j03/ex03/cookie_crisp.php
+++ j03/ex03/cookie_crisp.php
@@ -1,14 +1,20 @@
 <?php
-switch ($_GET["action"])
+if (isset($_GET["action"]) && isset($_GET["name"]))
 {
-	case "set":
-		setcookie($_GET["name"], $_GET["value"], time() + (10 * 365 * 24 * 60 * 60));
-		break;
-	case "get":
-		echo $_COOKIE[$_GET["name"]];
-		break;
-	case "del":
-		setcookie($_GET["name"], $_COOKIE[$_GET["name"]], time() - 5000000);
-		break;
+	switch ($_GET["action"])
+	{
+		case "set":
+			if (isset($_GET["value"]))
+				setcookie($_GET["name"], $_GET["value"], time() + (10 * 365 * 24 * 60 * 60));
+			break;
+		case "get":
+			if (isset($_COOKIE[$_GET["name"]]))
+				echo $_COOKIE[$_GET["name"]];
+			break;
+		case "del":
+			if (isset($_COOKIE[$_GET["name"]]))
+				setcookie($_GET["name"], $_COOKIE[$_GET["name"]], time() - 5000000);
+			break;
+	}
 }
 ?>
